ea13d74d73f8c0294a7a2601ed82f3da19d5e222
ws123 <372532647@qq.com>
2015-05-28 16:14:43 +0800

    Initial commit

---
+++ app/src/main/java/cc/carlos/applicaion/BaseApplication.java
@@ -0,0 +1,13 @@
+package cc.carlos.applicaion;
+
+import android.app.Application;
+
+/**
+ * Created by zhangheilong on 15/5/28.
+ */
+public class BaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
